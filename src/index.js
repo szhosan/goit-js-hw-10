343694ce4b16c3aa85f2d9c732eaf282675b9c5c
@s_zhsn <s.zhosan@gmail.com>
2022-03-06 18:50:41 +0200

    h-w-10-ready

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 
+export default function showError() {
+  Notify.failure('Oops, there is no country with that name');
+}
+
 const refs = {
   inputRef: document.querySelector(`#search-box`),
   ulRef: document.querySelector('.country-list'),
@@ -17,38 +21,32 @@ function onInput(e) {
   clearMarkup();
   if (requestText.length === 0) {
     return;
-  }  
-  fetchCountries(requestText)
-    .then(result => {
-      if (result.status === 404) {
-        Notify.failure('Oops, there is no country with that name');
-        return;
-      }
-      if (result.length <= 10) {
-        createListMarkup(result);
-      } else Notify.info('Too many matches found. Please enter a more specific name.');
-      if (result.length === 1) {
-        createCountryInfoMarkup(result[0]);
-      }
-    })
-    .catch(e => console.log(`CATCH: ${e}`));
+  }
+  fetchCountries(requestText).then(result => {
+    if (result.length <= 10) {
+      createListMarkup(result);
+    } else Notify.info('Too many matches found. Please enter a more specific name.');
+    if (result.length === 1) {
+      createCountryInfoMarkup(result[0]);
+    }
+  });
 }
 
 function createListMarkup(countryList) {
   let markup = '';
-  for (const country of countryList) {
+  countryList.map(country => {
     markup += `<li class="country-item"><img class="country-image" src="${country.flags.svg}" alt="${country.name.official} flag">${country.name.official}</li>`;
-  }
+  });
   refs.ulRef.innerHTML = markup;
 }
 
 function createCountryInfoMarkup(country) {
   let languages = '';
-  for (let language of Object.values(country.languages)) {
+  Object.values(country.languages).map(language => {
     languages += language + ', ';
-  }
+  });
   languages = languages.slice(0, languages.length - 2);
-  let markup = `<p><span>Capital: </span>${country.capital}</p>
+  const markup = `<p><span>Capital: </span>${country.capital}</p>
   <p><span>Population: </span>${country.population}</p>
   <p><span>Languages: </span>${languages}</p>`;
   refs.infoDivRef.innerHTML = markup;
